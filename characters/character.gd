7957c8088e1172a26c3c6d4924174ba8f90f63ca
Cauã Rodrigues <cauarodrigues066@gmail.com>
2025-07-06 20:01:58 -0300

    Criação das cenas dos personagens e remoção de cenas/nodes não utilizados

--- characters/character.gd
+++ characters/character.gd
@@ -1,1 +1,22 @@
 extends Node2D
+class_name Character
+
+var id = ""
+var character_name = ""
+var type = ""
+var max_hp : int
+var current_hp : int
+var speed : int
+var actions = []
+var status_effects = []
+
+
+func setup(data: Dictionary):
+	id = data.get("id", "")
+	character_name = data.get("name", "")
+	type = data.get("type", "")
+	max_hp = data.get("max_hp", 0)
+	current_hp = max_hp
+	speed = data.get("speed", 0)
+	actions = data.get("actions", [])
+	$Sprite2D.texture = data.get("texture", null)
